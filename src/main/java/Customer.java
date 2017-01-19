0a1493b5777b4d23dff80530287c617e6157a66c
Lobo Chu <Lobo.Chu@audatex.com>
2017-01-19 12:12:21 +0800

    replace method  with stream function

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Customer {
   private final List<Rental> rentals = new ArrayList<>();
@@ -15,31 +17,62 @@ public class Customer {
     rentals.add(rental);
   }
 
-  private String getName() {
-    return name;
-  }
-
   public String generateStatement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
-    String result = "Rental Record for " + getName() + "\n";
-
-    for (Rental rental : rentals) {
-      double rentalPrice = tariff.calculatePrice(rental);
-
-      frequentRenterPoints += loyaltyPlan.calculatePoints(rental);
-
-      result += "\t" + rental.getMovie().getTitle() + "\t"
-          + String.valueOf(rentalPrice) + "\n";
-      totalAmount += rentalPrice;
-
-    }
-
-    result += "You owed " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
-
-
-    return result;
+    final StatementGenerator statementGenerator = new StatementGenerator();
+    return statementGenerator.generate(name, rentals, new PricePlan());
   }
 
+
+  public class PriceRental {
+    public final double price;
+    public final int points;
+    public final String title;
+
+    public PriceRental(double price, int points, String title) {
+      this.price = price;
+      this.points = points;
+      this.title = title;
+    }
+  }
+
+  private class PricePlan implements Function<Rental, PriceRental> {
+    @Override
+    public PriceRental apply(Rental r) {
+      return new PriceRental(
+          tariff.calculatePrice(r),
+          loyaltyPlan.calculatePoints(r),
+          r.getMovie().getTitle()
+      );
+    }
+  }
+
+  public static class StatementGenerator {
+
+
+    public String generate(String name, List<Rental> rentals, PricePlan plan) {
+      double totalAmount = 0;
+      int frequentRenterPoints = 0;
+      String result = "Rental Record for " + name + "\n";
+
+      final List<PriceRental> priced = rentals.stream().map(plan).collect(Collectors.toList());
+
+      for (PriceRental priceRental : priced) {
+
+        double rentalPrice = priceRental.price;
+
+        frequentRenterPoints += priceRental.points;
+
+        result += "\t" + priceRental.title + "\t"
+            + String.valueOf(rentalPrice) + "\n";
+        totalAmount += rentalPrice;
+
+      }
+
+      result += "You owed " + String.valueOf(totalAmount) + "\n";
+      result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+
+
+      return result;
+    }
+  }
 }
