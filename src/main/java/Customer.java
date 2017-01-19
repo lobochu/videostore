2febd2e3ef0431689eada48a9eaaba4a22d4e4b2
Lobo Chu <Lobo.Chu@audatex.com>
2017-01-19 11:21:41 +0800

    Extract class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-  private final String name;
   private final List<Rental> rentals = new ArrayList<>();
+  private final Tariff tariff = new Tariff();
+  private final LoyaltyPlan loyaltyPlan = new LoyaltyPlan();
+  private final String name;
 
   public Customer(String name) {
     this.name = name;
@@ -17,40 +19,19 @@ public class Customer {
     return name;
   }
 
-  public String statement() {
+  public String generateStatement() {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
 
     for (Rental rental : rentals) {
-      double thisAmount = 0;
+      double rentalPrice = tariff.calculatePrice(rental);
 
-      // determines the amount for each line
-      switch (rental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (rental.getDaysRented() > 2)
-            thisAmount += (rental.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += rental.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (rental.getDaysRented() > 3)
-            thisAmount += (rental.getDaysRented() - 3) * 1.5;
-          break;
-      }
-
-      frequentRenterPoints++;
-
-      if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-          && rental.getDaysRented() > 1)
-        frequentRenterPoints++;
+      frequentRenterPoints += loyaltyPlan.calculatePoints(rental);
 
       result += "\t" + rental.getMovie().getTitle() + "\t"
-          + String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+          + String.valueOf(rentalPrice) + "\n";
+      totalAmount += rentalPrice;
 
     }
 
@@ -59,5 +40,41 @@ public class Customer {
 
 
     return result;
+  }
+
+  private class Tariff {
+    private double calculatePrice(Rental rental) {
+      double rentalPrice = 0;
+
+      // determines the amount for each line
+      switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          rentalPrice += 2;
+          if (rental.getDaysRented() > 2)
+            rentalPrice += (rental.getDaysRented() - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          rentalPrice += rental.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          rentalPrice += 1.5;
+          if (rental.getDaysRented() > 3)
+            rentalPrice += (rental.getDaysRented() - 3) * 1.5;
+          break;
+      }
+      return rentalPrice;
+    }
+  }
+
+  private class LoyaltyPlan {
+    private int calculatePoints(Rental rental) {
+      int frequentRenterPoints = 0;
+      frequentRenterPoints++;
+
+      if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+          && rental.getDaysRented() > 1)
+        frequentRenterPoints++;
+      return frequentRenterPoints;
+    }
   }
 }
--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -8,20 +8,20 @@ public class VideoStoreTest {
   @Test
   public void testSingleNewReleaseStatement() {
     customerRentsMovieForPeriod(newRelease("The Cell"), 3);
-    Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
+    Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.generateStatement());
   }
 
   @Test
   public void testDualNewReleaseStatement() {
     customerRentsMovieForPeriod(newRelease("The Cell"), 3);
     customerRentsMovieForPeriod(newRelease("The Tigger Movie"), 3);
-    Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
+    Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.generateStatement());
   }
 
   @Test
   public void testSingleChildrensStatement() {
     customerRentsMovieForPeriod(children("The Tigger Movie"), 3);
-    Assert.assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
+    Assert.assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.generateStatement());
   }
 
   @Test
@@ -30,7 +30,7 @@ public class VideoStoreTest {
     customerRentsMovieForPeriod(regularMovie("8 1/2"), 2);
     customerRentsMovieForPeriod(regularMovie("Eraserhead"), 3);
 
-    Assert.assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement());
+    Assert.assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.generateStatement());
   }
 
   private void customerRentsMovieForPeriod(Movie movie, int daysRented) {
