d0a6d04df30545f6c99f19333f7ea03bfa8a4713
Lobo Chu <Lobo.Chu@audatex.com>
2017-02-21 10:29:52 +0800

    collect testst with functional style

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,9 +1,37 @@
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class VideoStoreTest {
 
   private final Customer customer = new Customer("Fred", new PricePlan(new LoyaltyPlan(), new Tariff()));
+
+  @Test
+  public void basic_tariff() throws Exception {
+    Customer basic = new Customer("basic", new PricePlan(new LoyaltyPlan(), new Tariff()));
+    basic.addRental(new Rental(children("childrens"), 3));
+    basic.addRental(new Rental(regularMovie("regular"), 3));
+    basic.addRental(new Rental(newRelease("new"), 3));
+
+    assertThat(basic.generateStatement(new StatementGenerator()),
+        hasStatementLines(
+            "Rental record for basic",
+            "\tchildrens\t1.5",
+            "\tregular\t3.5",
+            "\tnew\t9.0",
+            "You owed 14.0",
+            "You earned 4 frequent renter points"
+        )
+    );
+  }
+
+  private org.hamcrest.Matcher<String> hasStatementLines(String... lines) {
+    return equalTo(stream(lines).collect(joining("\n", "", "\n")));
+
+  }
 
   @Test
   public void testSingleNewReleaseStatement() {
